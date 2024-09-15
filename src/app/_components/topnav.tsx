800db2e7f9a91e45119429cd6c04775f67999c0a
Maxwell paulo <maxpaulo.mps@gmail.com>
2024-09-15 17:11:33 -0300

    prepare to use routes

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,9 +1,15 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
 export function Topnav() {
+    const router = useRouter()
+
     return (
         <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
             <div>Home Page</div>
 
-            <div className="flex flex-row items-center gap-4">
+            <div className="flex flex-row items-center gap-4" onClick={() => { router.refresh() }}>
                 Add new Project
             </div>
         </nav>

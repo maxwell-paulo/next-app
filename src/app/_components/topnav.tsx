2dd1f4a5af2c75b2b4f8cdbe42ed10c45bccd596
Maxwell paulo <maxpaulo.mps@gmail.com>
2024-09-15 16:49:12 -0300

    refactor topnav and homepage

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,11 @@
+export function Topnav() {
+    return (
+        <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+            <div>Home Page</div>
+
+            <div className="flex flex-row items-center gap-4">
+                Add new Project
+            </div>
+        </nav>
+    )
+} 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,28 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic"
 
-
-export default async function HomePage() {
+async function Contents() {
   const contents = await db.query.contents.findMany()
 
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {contents.map((content) => (
+    <div className="flex flex-wrap gap-4">
+      {contents.map((content) => (
+        <div className="flex w-48 flex col" key={content.id}>
+          <div key={content.id}>{content.id}</div>
           <div key={content.id}>{content.name}</div>
-        ))}
-      </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+
+export default async function HomePage() {
+
+
+  return (
+    <main className="">
+      <Contents />
     </main>
   );
 }

82bb0a0a65cdadc0ea6fb032528e2d07a3d6c7f1
Maxwell paulo <maxpaulo.mps@gmail.com>
2024-09-15 15:39:34 -0300

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import { db } from "~/server/db";
 
 
-export default function HomePage() {
+export default async function HomePage() {
+  const contents = await db.query.contents.findMany()
+
   return (
     <main className="">
-      <div>teste</div>
+      <div className="flex flex-wrap gap-4">
+        {contents.map((content) => (
+          <div key={content.id}>{content.name}</div>
+        ))}
+      </div>
     </main>
   );
 }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,21 +8,16 @@ import {
   serial,
   timestamp,
   varchar,
+  integer,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `next-app_${name}`);
+export const createTable = pgTableCreator((name) => `next_app_${name}`);
 
-export const posts = createTable(
-  "post",
+export const projects = createTable(
+  "projects",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
@@ -30,7 +25,42 @@ export const posts = createTable(
       () => new Date()
     ),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
+  (projects) => ({
+    nameIndex: index("name_idx").on(projects.name),
+  })
+);
+
+export const contents = createTable(
+  "contents",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }).notNull(),
+    text: varchar("text", { length: 1000 }).notNull(),
+    projectId: integer("project_id").references(() => projects.id).notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (contents) => ({
+    projectIdIndex: index("project_id_idx").on(contents.projectId),
+  })
+);
+
+export const dynamicFields = createTable(
+  "dynamic_fields",
+  {
+    id: serial("id").primaryKey(),
+    contentId: integer("content_id").references(() => contents.id).notNull(),
+    key: varchar("key", { length: 256 }).notNull(),
+    value: varchar("value", { length: 1000 }).notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+  },
+  (dynamicFields) => ({
+    contentIdIndex: index("content_id_idx").on(dynamicFields.contentId),
   })
 );

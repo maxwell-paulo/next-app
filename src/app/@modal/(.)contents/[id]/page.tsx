7983bc5f51c092780087de20c5425462bc13f643
Maxwell paulo <maxpaulo.mps@gmail.com>
2024-09-17 21:46:37 -0300

    remove unused aguments

--- src/app/@modal/(.)contents/[id]/page.tsx
+++ src/app/@modal/(.)contents/[id]/page.tsx
@@ -2,17 +2,11 @@ import { Modal } from "./modal"
 import FullPageContentView from "~/app/common/full-page-content-view"
 
 
-export default function ContentModal({
-    params: { id: contentId },
-}: {
-    params: { id: string }
-}) {
-    const idAsNumber = Number(contentId)
-    if (Number.isNaN(idAsNumber)) throw new Error("Invalid content id")
+export default function ContentModal() {
 
     return (
         <Modal>
-            <FullPageContentView id={idAsNumber} />
+            <FullPageContentView />
         </Modal>
     )
 }
--- src/app/contents/[id]/page.tsx
+++ src/app/contents/[id]/page.tsx
@@ -1,15 +1,9 @@
 import FullPageContentView from "~/app/common/full-page-content-view"
 
 
-export default function ContentPage({
-    params: { id: contentId },
-}: {
-    params: { id: string }
-}) {
-    const idAsNumber = Number(contentId)
-    if (Number.isNaN(idAsNumber)) throw new Error("Invalid content id")
+export default function ContentPage() {
 
     return (
-        <FullPageContentView id={idAsNumber} />
+        <FullPageContentView />
     )
 }

19aa91882c4dfa648ee4bfd86f44f346965eecf4
Maxwell paulo <maxpaulo.mps@gmail.com>
2024-09-17 21:44:51 -0300

    close content modal

--- src/app/@modal/(.)contents/[id]/modal.tsx
+++ src/app/@modal/(.)contents/[id]/modal.tsx
@@ -3,6 +3,7 @@
 import { type ElementRef, useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { createPortal } from "react-dom";
+import { XCircleIcon } from "@heroicons/react/24/outline";
 
 export function Modal({ children }: { children: React.ReactNode }) {
     const router = useRouter();
@@ -25,7 +26,13 @@ export function Modal({ children }: { children: React.ReactNode }) {
             onClose={onDismiss}
         >
             {children}
-            {/* <button onClick={onDismiss} className="close-button" /> */}
+            <button
+                onClick={onDismiss}
+                className="absolute top-4 right-4 p-2 text-gray-600 hover:text-gray-800"
+                aria-label="Close"
+            >
+                <XCircleIcon className="h-6 w-6" />
+            </button>
         </dialog>,
         document.getElementById("modal-root")!,
     );
